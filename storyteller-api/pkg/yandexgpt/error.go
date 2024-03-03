26288264fc8c6473ff86b41b9e45935ac8b80e82
sheeiavellie <josephlandmars@gmail.com>
2024-03-03 20:56:41 +0300

    fix: remove grpc status from yandexgpt error

--- storyteller-api/pkg/yandexgpt/error.go
+++ storyteller-api/pkg/yandexgpt/error.go
@@ -1,7 +1,6 @@
 package yandexgpt
 
 type YandexGPTError struct {
-	GRPCCode   int    `json:"grpcCode"`
 	HTTPCode   int    `json:"httpCode"`
 	Message    string `json:"message"`
 	HTTPStatus string `json:"httpStatus"`
