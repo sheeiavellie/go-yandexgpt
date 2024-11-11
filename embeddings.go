3703c7fdbf1824dfe37538218eb118f7ae91f152
Ann Vasileva <a.vasileva@bpo.travel>
2024-11-12 01:55:37 +0300

    chore: add comments for opreation and embeddings

--- embeddings.go
+++ embeddings.go
@@ -20,6 +20,14 @@ type embeddingsModel struct {
 func (em embeddingsModel) String() string {
 	return fmt.Sprint(em.modelName)
 }
+
+// Get embeddings from Yandex foundation models.
+//
+// If you're using IAM token, make sure to update client's IAM token by calling
+// GetIAMToken(iamToken string) method first.
+//
+// Keep in mind that if for some strange reason you provided API key and IAM token to the client,
+// this method will use API key.
 func (c *YandexGPTClient) GetEmbedding(
 	ctx context.Context,
 	request YandexGPTEmbeddingsRequest,
